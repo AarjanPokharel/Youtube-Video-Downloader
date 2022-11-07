f3fb2fb5e489082918aa03029f6d0fcd5103357a
AarjanPokharel <104973899+AarjanPokharel@users.noreply.github.com>
2022-11-07 17:38:15 +0545

    initial commit

---
+++ youtubeDownloader.py
@@ -0,0 +1,16 @@
+from pytube import YouTube
+from sys import argv
+
+link = argv[1]
+yt = YouTube(link)
+
+print(f"Title: {yt.title}")
+
+print(f"Views: {yt.views}")
+
+yd = yt.streams.get_highest_resolution()
+
+yd.download('D:\Youtube Videos')
+
+
+
